702abb41067e845126768efe206a84b7a1181ec6
shanecsj <44423456+shanecsj@users.noreply.github.com>
2021-03-27 21:42:43 +0800

    Added UI overlay to main scene

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    bool active = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +15,19 @@ public class MenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Oculus_CrossPlatform_Button2") > 0
-            || Input.GetKeyDown(KeyCode.A))
+        if (Input.GetAxis("Oculus_CrossPlatform_Button4") > 0 || Input.GetKeyDown(KeyCode.B))
         {
-            if (!active) {
-                transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
-                active = true;
-            } else {
-                transform.localScale = new Vector3(0, 0, 0);
-                active = false;
-            }
+            transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
         }
+    }
+
+    public void CloseMenu()
+    {
+        transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    public void ResetComponents()
+    {
+        SceneManager.LoadScene(1);
     }
 }
